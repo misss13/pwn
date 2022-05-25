75955e3c14c63522a46f9947c9e3c6a281e15bef
Misss13 <zuzia.konopek@gmail.com>
2022-05-26 00:45:05 +0200

    skel updated

--- serwer/sbin/defines.py
+++ serwer/sbin/defines.py
@@ -44,12 +44,67 @@ def parsing(SSID, password):
     """Parsing txt to format"""
     return str(SSID) + ":" + str(password)
 
+def check_ssid():
+    """Checking if rainbow table for ssid is aviale SSID.txt"""
+
+def download_rainbow():
+    """Connecting and downloading rainbow tables"""
+    #https://freerainbowtables.com/
+    #https://www.renderlab.net/projects/WPA-tables/ <- wybraned
+    #chyba skipne bo to duże rzeczy są trzeba pobrać wcześniej
+
+def rainbow_tables_checking():
+    """Use fast rainbow table"""
+
+
+def check_dictionary():
+    """Check if dictionary.txt exist"""
+
+
+def compute_aircrack_dictionary():
+    """Aircrack checking on the run based on dictionary"""
+
+
+def compute_cat_bruteforce():
+    """Hashcat hashcat rainbow table bruteforcing"""
+
+
+def send_end(ssid, password, socket):
+    """Sending end ending session"""
+    if password != None:
+        sending(socket, parsing(ssid, password))
+        socket.close()
+        return True
+
 
 def client_hello(socket, address):
     """Define handling client connection"""
-    #tests for netcat + os commands
+
     r = reciving(socket) 
     print(r)
-    sending(socket, "hejo")
-    os.system("echo yooo")
-    socket.close()
+    if(str(r) == "yoo"):
+    #proste potwierdzenie że klient i serwer działa
+        sending(socket, "oii")
+        print("send")
+        ssid = reciving(socket)
+
+        ##Main function
+        #phaze 1. checking if password exist in downloaded rainbow table and using it
+        if check_ssid(ssid) == True:
+            download_rainbow()
+            password = rainbow_tables_checking()
+            if send_end(ssid, password, socket) == True:
+                return True
+        
+        #phaze 2. realtime checking passwords from dictionary
+        password = compute_aircrack_dictionary(ssid)
+        if send_end(ssid, password, socket) == True:
+            return True
+        
+        #phaze 3. bruteforce with hashcat
+        password = compute_aircrack_dictionary(ssid)
+        if send_end(ssid, password, socket) == True:
+            return True
+    else:
+        #os.system("echo yooo")
+        socket.close()
